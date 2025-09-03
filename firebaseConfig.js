c68d39e22e63274efdbafb87c8be8a8070157178
KurtElemino <Kurt1elemino@gmail.com>
2025-09-03 09:23:45 +0800

    create goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,19 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-
-// Initialize Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  apiKey: "AIzaSyBLH6ZC2cDLpjyS-RJLc_2CRNOexfNObgU",
+  authDomain: "kre-goals.firebaseapp.com",
+  projectId: "kre-goals",
+  storageBucket: "kre-goals.firebasestorage.app",
+  messagingSenderId: "107491808488",
+  appId: "1:107491808488:web:9c604edbc83bb14e33255d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+export const db = getFirestore(app)
